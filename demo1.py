5c8d3ef31253a329325045797c7c65a00ea447de
Affandes <affandes@uin-suska.ac.id>
2019-06-22 15:34:38 +0700

    Save Kamus ke Single File (Modify demo3.py) Save Kamus ke Multifile (Run demo3.py) Update Kamus dari Tuple (Run demo1.py)

--- demo1.py
+++ demo1.py
@@ -2,20 +2,42 @@ import tagger.tagger as tg
 import kamus.kamus as km
 import nltk
 import time
+from nltk.tag.sequential import UnigramTagger
 
-terms = nltk.corpus.gutenberg.words("austen-emma.txt")
+terms = ['adu','abang','caca','abi','acara']
+
+nldict = UnigramTagger([[
+    ('a','n'),
+    ('abah','n'),
+    ('abang','n'),
+    ('abi','n'),
+    ('abu','n'),
+    ('acak','n'),
+    ('acar','n'),
+    ('acara','n'),
+    ('ancam','n'),
+]])
 
 my_kamus = km.Kamus()
 my_tagger = tg.Tagger(my_kamus)
 
 startx = time.time()
-my_tagger.tag(terms)
+my_result = my_tagger.tag(terms)
 endx = time.time()
 
 
 starty = time.time()
-nltk.pos_tag(terms)
+#nlresult = nldict.tag(terms)
 endy = time.time()
 
-print(endx-startx)
-print(endy-starty)
+#print(endx-startx)
+#print(endy-starty)
+
+my_kamus.update_kamus(('baru','a'))
+my_kamus.update_kamus(('basa','a'))
+my_kamus.update_kamus(('bata','n'))
+
+#print my_result
+#print "------------"
+#print nlresult
+my_kamus.save_kamus('simpan baru.txt')
